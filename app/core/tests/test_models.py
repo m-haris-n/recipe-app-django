a940f159296b6d848874d1d32837a69acb36cb7d
M. Haris Nabeel <m.haris.nabeel@gmail.com>
2023-07-01 18:24:18 +0500

    fixed linting error

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,7 +10,10 @@ class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         email = "test@example.com"
         password = "test@123"
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -24,18 +27,23 @@ class ModelTests(TestCase):
             ["Test4@EXAMPLE.COM", "Test4@example.com"],
         ]
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email, "sample123")
+            user = get_user_model().objects.create_user(
+                email,
+                "sample123",
+            )
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
         """test that creates a user without an email raises a ValueError"""
-
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user("", "test123")
 
     def test_create_superuser(self):
         """Test creating a superuser"""
-        user = get_user_model().objects.create_superuser("test@example.com", "test123")
+        user = get_user_model().objects.create_superuser(
+            "test@example.com",
+            "test123",
+        )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
